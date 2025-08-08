8275df9e77adb288883812b353ea59fb40da9b8f
rui <224653604+dysfunctional-illusionist@users.noreply.github.com>
2025-08-08 02:40:26 +0100

    jsconfig for aliases (paths are easier)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import path from 'path';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
