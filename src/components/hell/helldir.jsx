9703d21564c2430772878c34ef1fd3a68100c584
dys_illu <celestial.leds@gmail.com>
2026-07-29 01:07:19 +0100

    afterlife directory page fixed

--- src/components/hell/helldir.jsx
+++ src/components/hell/helldir.jsx
@@ -1,6 +1,21 @@
 
 // const buttons = JSON.parse(nav.dataset.buttons);
 
+                // border-2 border-red-700
+                // bg-black
+                // text-red-700
+
+                // hover:border-orange-500
+                // hover:bg-red-900
+                // hover:text-white
+                
+                // hover:from-black hover:via-red-900 hover:to-orange-600
+                // hover:text-white
+                // font-doHyeon text-4xl text-red-700 px-10 py-6
+                // rounded-lg shadow-lg
+
+                // transition-colors duration-500
+
 export default function HellDir_Buttons({ pages }) {
   return (
     <>
@@ -10,21 +25,7 @@ export default function HellDir_Buttons({ pages }) {
 
             <button
               onClick={() => window.location.href = page.url }
-              className="
-                font-doHyeon text-4xl text-red-700 px-10 py-6
-                rounded-lg shadow-lg
-
-                border-2 border-red-700
-                bg-black
-                text-red-700
-
-                hover:border-orange-500
-                hover:bg-red-900
-                hover:text-white
-                
-                hover:from-black hover:via-red-900 hover:to-orange-600
-                hover:text-white
-                transition-colors duration-500">
+              className="ui-button rounded-lg border-2 text-4xl">
               {/* <img src="/icons/star.svg" alt="Star icon" className="w-5 h-5" /> */}
               {page.title}
             </button>
